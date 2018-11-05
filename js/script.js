90370d4b0585fcff7802a28842a02a78a33d2baa
KlaKali1 <klaudia.m.kalinowska@gmail.com>
2018-11-05 21:06:58 +0100

    Gallery Panel Part // added markup

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var allPanels = document.querySelectorAll(".gallery__singlePanel");
+
+function openPanel() {
+    this.classList.toggle("singlePanel--open");
+}
+
+allPanels.forEach(singlePanel => singlePanel.addEventListener("mouseover", openPanel))
+
