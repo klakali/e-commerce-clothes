f8b7b16da350f04411cef6e0a4452486c9b1e989
KlaKali1 <klaudia.m.kalinowska@gmail.com>
2018-11-06 22:21:26 +0100

    Gallery
    - photos added
    - galerry text added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var allPanels = document.querySelectorAll(".gallery__singlePanel");
+var galleryTexts = document.querySelectorAll(".singlePanel__header--open");
 
 function openPanel() {
     this.classList.toggle("singlePanel--open");
 }
 
 allPanels.forEach(singlePanel => singlePanel.addEventListener("click", openPanel))
-
